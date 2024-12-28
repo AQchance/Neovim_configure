28ab26302055903989cd8968d42986f68ee0f0f2
AQchance <1363616727@qq.com>
2024-12-28 20:55:03 +0800

    初始化仓库

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+vim.wo.number = true
+vim.wo.relativenumber = false
+vim.opt.clipboard = "unnamedplus"
+vim.o.guifont = "JetBrainsMono Nerd Font:h16"
+vim.g.neovide_transparency = 0.85
+-- vim.g.neovide_window_blurred = true
+-- vim.g.neovide_background_image = "/home/anqian/Pictures/bg.jpg"
---
+++ lua/plugins/plugins.lua
@@ -0,0 +1,36 @@
+local lspconfig = require("lspconfig")
+lspconfig.clangd.setup({
+  cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+  init_options = {
+    fallbackFlags = { "-std=c++23" },
+  },
+})
+return {
+  -- add pyright to lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    ---@class PluginLspOpts
+    opts = {
+      ---@type lspconfig.options
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        clangd = {},
+      },
+    },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
+}
