4af9198758b5c35629d59cd1fb7eff0773ec8363
AQchance <1363616727@qq.com>
2025-01-04 12:38:03 +0800

    把透明背景关闭了，然后将Tab键的功能修改为了补全代码时的选择，与回车类似

--- init.lua
+++ init.lua
@@ -4,6 +4,6 @@ vim.wo.number = true
 vim.wo.relativenumber = false
 vim.opt.clipboard = "unnamedplus"
 vim.o.guifont = "JetBrainsMono Nerd Font:h16"
-vim.g.neovide_transparency = 0.85
+vim.g.neovide_transparency = 1
 -- vim.g.neovide_window_blurred = true
 -- vim.g.neovide_background_image = "/home/anqian/Pictures/bg.jpg"
